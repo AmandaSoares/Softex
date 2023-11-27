4066862bde511e09b9fc243396edc75549d846d0
AmandaSoares <amandags_soares@hotmail.com>
2023-11-27 12:00:49 -0300

    removendo pasta produto

--- produto/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize('nomedobanco', 'usuario', 'senha', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
